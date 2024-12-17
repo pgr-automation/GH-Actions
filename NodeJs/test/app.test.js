70e7f9e8f359abe8245e022a021e41911dc44c88
pgr-automation <grprashanth94@gmail.com>
2024-12-18 01:20:09 +0530

    new update

--- NodeJs/test/app.test.js
+++ NodeJs/test/app.test.js
@@ -1,6 +1,5 @@
 const request = require('supertest');
 const app = require('../src/app');
-// test
 describe('GET /', () => {
   it('should return a 200 status', async () => {
     const res = await request(app).get('/');
