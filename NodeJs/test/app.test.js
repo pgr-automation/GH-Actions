d6f31f178b689b40f94e9df690ed92f5b4edbde8
pgr-automation <grprashanth94@gmail.com>
2024-12-17 21:56:04 +0530

    test to fail

--- NodeJs/test/app.test.js
+++ NodeJs/test/app.test.js
@@ -5,6 +5,6 @@ describe('GET /', () => {
   it('should return a 200 status', async () => {
     const res = await request(app).get('/');
     expect(res.status).toBe(200);
-    expect(res.body.message).toBe('Welcome to the Node.js API');
+    expect(res.body.message).not.toBe('Welcome to the Node.js API');
   });
 });
